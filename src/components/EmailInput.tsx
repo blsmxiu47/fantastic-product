0c91796b755811c6f20bd1e8d673c245b48a60a4
Wes Warriner <fridgehistory@gmail.com>
2023-09-30 01:15:09 -0700

    limit email input area width on md+ screens

--- src/components/EmailInput.tsx
+++ src/components/EmailInput.tsx
@@ -1,7 +1,7 @@
 const EmailInput = () => {
     return (
         <input
-            className="border-2 border-gray-300 rounded my-2 md:mx-2 px-4 py-2 text-black text-sm lg:text-lg xl:text-xl min-w-0 w-full"
+            className="border-2 border-gray-300 rounded my-2 md:mx-2 px-4 py-2 text-black text-sm lg:text-lg xl:text-xl min-w-0 w-full md:w-96"
             type="email"
             placeholder="Work email"
         />
